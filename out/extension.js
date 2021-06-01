c563dc4164a80d705cc98e5eca3fda92c4d98232
SPdigger <SPdigger@yandex.ru>
2021-06-02 02:20:47 +0300

    Release v1.1
    Добавлена поддержка rgb(a) и hsl(a)

--- out/extension.js
+++ out/extension.js
@@ -15,6 +15,7 @@ function activate(context) {
         }
         decTypeArray.length = 0;
         const regHex = /(?:\#|\b0x)([a-f0-9]{6}([a-f0-9]{2})?)/gi;
+        const regCSS = /((rgb|hsl)a?\([\d]{1,3},\s*[\d]{1,3}%?,\s*[\d]{1,3}%?(,\s*\d?\.?\d+)?\))/gi;
         const text = activeEditor.document.getText();
         const bigArray = [];
         let match;
@@ -31,6 +32,28 @@ function activate(context) {
                 borderStyle: 'solid',
                 borderColor: matchHex
             });
+            const tmp = [];
+            tmp.push(decoration);
+            console.log(match[0].length);
+            bigArray.push(tmp);
+            decTypeArray.push(hexDecorationType);
+        }
+        while ((match = regCSS.exec(text))) {
+            const startPos = activeEditor.document.positionAt(match.index);
+            const endPos = activeEditor.document.positionAt(match.index + match[0].length);
+            
+            const decoration = {
+                range: new vscode.Range(startPos, endPos)
+            };
+
+            let matchCSS = match[0];
+            console.log(matchCSS);
+            const hexDecorationType = vscode.window.createTextEditorDecorationType({
+                borderWidth: '2px',
+                borderStyle: 'solid',
+                borderColor: matchCSS
+            });
+            
             const tmp = [];
             tmp.push(decoration);
             console.log(match[0].length);
